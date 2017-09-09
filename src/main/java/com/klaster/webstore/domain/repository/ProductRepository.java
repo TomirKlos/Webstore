923348e6dfc9307f848a63d243cbf63c30f9e867
TomirKlos <31741223+TomirKlos@users.noreply.github.com>
2017-09-10 01:44:55 +0200

    added CRUD

--- src/main/java/com/klaster/webstore/domain/repository/ProductRepository.java
+++ src/main/java/com/klaster/webstore/domain/repository/ProductRepository.java
@@ -8,6 +8,10 @@ import java.util.List;
  * Created by MSI DRAGON on 2017-09-09.
  */
 public interface ProductRepository {
+    void create(Product product);
+    Product read(long productId);
+  //  Product update(Product product);
+    void delete(Product product);
 
     List<Product> getAllProducts();
 }
