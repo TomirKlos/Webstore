3f843539e3b6de96717bcdef8b95391c2a96f20d
TomirKlos <31741223+TomirKlos@users.noreply.github.com>
2017-09-10 01:45:48 +0200

    created interface productService

---
+++ src/main/java/com/klaster/webstore/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.klaster.webstore.service;
+
+import com.klaster.webstore.domain.Product;
+import com.klaster.webstore.domain.repository.ProductRepository;
+
+import java.util.List;
+
+/**
+ * Created by MSI DRAGON on 2017-09-09.
+ */
+public interface ProductService extends ProductRepository {
+
+}
