1fd560af5a47183d8e62669d5ab57e36bb408d41
TomirKlos <31741223+TomirKlos@users.noreply.github.com>
2017-09-28 13:18:12 +0200

    added method 'validate' to CartService

--- src/main/java/com/klaster/webstore/service/CartService.java
+++ src/main/java/com/klaster/webstore/service/CartService.java
@@ -1,9 +1,11 @@
 package com.klaster.webstore.service;
 
+import com.klaster.webstore.domain.Cart;
 import com.klaster.webstore.domain.repository.CartRepository;
 
 /**
  * Created by MSI DRAGON on 2017-09-27.
  */
 public interface CartService extends CartRepository {
+    Cart validate(String cartId);
 }
--- src/main/java/com/klaster/webstore/service/impl/CartServiceImpl.java
+++ src/main/java/com/klaster/webstore/service/impl/CartServiceImpl.java
@@ -2,6 +2,7 @@ package com.klaster.webstore.service.impl;
 
 import com.klaster.webstore.domain.Cart;
 import com.klaster.webstore.domain.repository.CartRepository;
+import com.klaster.webstore.exception.InvalidCartException;
 import com.klaster.webstore.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -26,5 +27,13 @@ public class CartServiceImpl implements CartService {
     }
     public void delete(String cartId) {
         cartRepository.delete(cartId);
+    }
+
+    public Cart validate(String cartId) {
+        Cart cart = cartRepository.read(cartId);
+        if(cart==null || cart.getCartItems().size()==0) {
+            throw new InvalidCartException(cartId);
+        }
+        return cart;
     }
 }
