51803bd6cead21ccbf6e220b5bee2dec9b60f769
TomirKlos <31741223+TomirKlos@users.noreply.github.com>
2017-10-04 23:13:37 +0200

    updated User class to handle new userManagement utility

--- src/main/java/com/klaster/webstore/domain/User.java
+++ src/main/java/com/klaster/webstore/domain/User.java
@@ -1,51 +1,74 @@
 package com.klaster.webstore.domain;
 
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.io.Serializable;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
+import org.hibernate.validator.constraints.NotEmpty;
 /**
- * Created by MSI DRAGON on 2017-09-30.
+ * Created by MSI DRAGON on 2017-10-02.
  */
+
+
 @Entity
-public class User implements Serializable {
-    private static final long serialVersionUID = 1398264706703526122L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long userId;
-    private String username;
+@Table(name="APP_USER")
+public class User implements Serializable{
+
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty
+    @Column(name="SSO_ID", unique=true, nullable=false)
+    private String ssoId;
+
+    @NotEmpty
+    @Column(name="PASSWORD", nullable=false)
     private String password;
-    private int noOfOrdersMade;
-    private String name;
-    private String mail;
 
-    public User() {
-        super();
+    @NotEmpty
+    @Column(name="FIRST_NAME", nullable=false)
+    private String firstName;
+
+    @NotEmpty
+    @Column(name="LAST_NAME", nullable=false)
+    private String lastName;
+
+    @NotEmpty
+    @Column(name="EMAIL", nullable=false)
+    private String email;
+
+    @NotEmpty
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "APP_USER_USER_PROFILE",
+            joinColumns = { @JoinColumn(name = "USER_ID") },
+            inverseJoinColumns = { @JoinColumn(name = "USER_PROFILE_ID") })
+    private Set<UserProfile> userProfiles = new HashSet<UserProfile>();
+
+    public Integer getId() {
+        return id;
     }
 
-    public User(String username, String password, String name, String mail) {
-        this.username = username;
-        this.password = password;
-        this.name = name;
-        this.mail = mail;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-    public long getUserId() {
-        return userId;
+    public String getSsoId() {
+        return ssoId;
     }
 
-    public void setUserId(long userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
+    public void setSsoId(String ssoId) {
+        this.ssoId = ssoId;
     }
 
     public String getPassword() {
@@ -56,49 +79,80 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    public int getNoOfOrdersMade() {
-        return noOfOrdersMade;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setNoOfOrdersMade(int noOfOrdersMade) {
-        this.noOfOrdersMade = noOfOrdersMade;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getName() {
-        return name;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
-    public String getMail() {
-        return mail;
+    public String getEmail() {
+        return email;
     }
 
-    public void setMail(String mail) {
-        this.mail = mail;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
+    public Set<UserProfile> getUserProfiles() {
+        return userProfiles;
+    }
 
-        User user = (User) o;
-
-        if (getUserId() != user.getUserId()) return false;
-        if (!getUsername().equals(user.getUsername())) return false;
-        if (!getName().equals(user.getName())) return false;
-        return getMail().equals(user.getMail());
+    public void setUserProfiles(Set<UserProfile> userProfiles) {
+        this.userProfiles = userProfiles;
     }
 
     @Override
     public int hashCode() {
-        int result = (int) (getUserId() ^ (getUserId() >>> 32));
-        result = 31 * result + getUsername().hashCode();
-        result = 31 * result + getName().hashCode();
-        result = 31 * result + getMail().hashCode();
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((ssoId == null) ? 0 : ssoId.hashCode());
         return result;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof User))
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (ssoId == null) {
+            if (other.ssoId != null)
+                return false;
+        } else if (!ssoId.equals(other.ssoId))
+            return false;
+        return true;
+    }
+
+    /*
+     * DO-NOT-INCLUDE passwords in toString function.
+     * It is done here just for convenience purpose.
+     */
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", ssoId=" + ssoId + ", password=" + password
+                + ", firstName=" + firstName + ", lastName=" + lastName
+                + ", email=" + email + "]";
+    }
+
+
+
 }
