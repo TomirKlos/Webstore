8f4e7827b248ecfaf9b49e7f7191065275d911b8
TomirKlos <31741223+TomirKlos@users.noreply.github.com>
2017-10-04 23:32:38 +0200

    added security configuration class, initializer

---
+++ src/main/java/com/klaster/webstore/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,15 @@
+package com.klaster.webstore.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+/**
+ * Created by MSI DRAGON on 2017-10-02.
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
